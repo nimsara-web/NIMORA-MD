257c55827be05043ac5c6f44299afc7c0dbd1b81
Nimsara <nimsaranethmintha@gmail.com>
2026-09-22 23:43:31 +0530

    Create _state.js

---
+++ plugins/game/_state.js
@@ -0,0 +1,7 @@
+// Shared game state
+const xoGames = new Map();
+const guessGames = new Map();
+const hangmanGames = new Map();
+const scrambleGames = new Map();
+
+module.exports = { xoGames, guessGames, hangmanGames, scrambleGames };
