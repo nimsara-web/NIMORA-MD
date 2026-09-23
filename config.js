1a5a55f65b34d1a4e8939223d6c37e6a6283f03b
Nimsara <nimsaranethmintha@gmail.com>
2026-09-24 01:13:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ module.exports = {
     // 🤖 BOT BRANDING
     // ==========================================
     botName: process.env.BOT_NAME || 'NIMORA MD',
-    botImageUrl: process.env.BOT_IMAGE_URL || 'https://raw.githubusercontent.com/nimsara-web/Im-Nim/refs/heads/main/Nimoradata/WhatsApp%20Image%202026-09-23%20at%201.57.30%20AM.jpeg',
+    botImageUrl: process.env.BOT_IMAGE_URL || 'https://raw.githubusercontent.com/nimsara-web/Im-Nim/refs/heads/main/Data/New/WhatsApp%20Image%202026-09-24%20at%201.11.32%20AM.jpeg',
     botAudioUrl: process.env.BOT_AUDIO_URL || 'https://github.com/nimsara-web/Im-Nim/raw/refs/heads/main/Nimoradata/0923.MP3',
     websiteLogoUrl: process.env.WEBSITE_LOGO_URL || 'https://raw.githubusercontent.com/nimsara-web/Im-Nim/refs/heads/main/Nimoradata/WhatsApp%20Image%202026-09-23%20at%201.57.30%20AM.jpeg',
 
